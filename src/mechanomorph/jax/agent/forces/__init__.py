346b965a7f95e9f0bb6467f82b536df0dcfceb06
Kevin <kevin.yamauchi@gmail.com>
2025-06-05 14:31:11 +0200

    add locomotion

--- src/mechanomorph/jax/agent/forces/__init__.py
+++ src/mechanomorph/jax/agent/forces/__init__.py
@@ -4,5 +4,10 @@ from mechanomorph.jax.agent.forces._cell_cell import (
     cell_cell_adhesion_potential,
     cell_cell_repulsion_potential,
 )
+from mechanomorph.jax.agent.forces._locomotion import biased_random_locomotion_force
 
-__all__ = ["cell_cell_adhesion_potential", "cell_cell_repulsion_potential"]
+__all__ = [
+    "biased_random_locomotion_force",
+    "cell_cell_adhesion_potential",
+    "cell_cell_repulsion_potential",
+]
